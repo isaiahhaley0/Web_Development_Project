d7eb87455ab7e4348bde0e468352d63517cf164b
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-03-30 02:09:07 -0600

    layout
    added initial components for layout.ejs
    added a few missing files.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ userController = require("./controllers/usersController");
 layouts = require("express-ejs-layouts");
 mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/confetti_cuisine",{useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb://localhost:27017/vibezdb",{useNewUrlParser: true, useUnifiedTopology: true })
 
 app.set("port",process.env.PORT||3000);
 
@@ -16,9 +16,7 @@ app.set("view engine", "ejs");
 app.use(layouts);
 
 
-app.get("/",function (req, res) {
-    res.send('About this wiki');
-})
+app.get("/",homecontroller.showIndex) 
 app.use(express.static("public"))
 app.use(
     express.urlencoded({
