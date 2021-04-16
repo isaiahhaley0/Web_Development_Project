88d29e65cca858a2b59b3d70b571d747226f0a71
ethanwalton <ejwalton17@yahoo.com>
2021-04-15 21:14:47 -0600

    schema changes

--- models/user.js
+++ models/user.js
@@ -1,9 +1,64 @@
-const mongoose = require("mongoose"),
-    userSchema = mongoose.Schema({
-        username: String, 
-        email: String, 
-        password: String
-    });
+const { TOO_MANY_REQUESTS } = require("http-status-codes")
+const mongoose = require("mongoose"), 
+{ Schema } = require ("mongoose"), 
+Subscriber = require("./subscriber")
+userSchema = new Schema(
+    {
+        name: {
+            first: {
+                type: String, 
+                required: true
+            },
+            last: {
+                type: String, 
+                required: true
+            }
+        },
+        email: {
+            type: String, 
+            required: true, 
+            unique: true
+        },
+        zipCode: {
+            type: Number, 
+            min: [10000, "Zip code too short"],
+            max: [99999]
+        },
+        password: {
+            type: String, 
+            required: true
+        },
+        //courses: [{type: Schema.Types.ObjectId, ref: Course}],
+        subscribedAccount: {type: Schema.Types.ObjectId, ref: Subscriber}
+    },
+    {
+        timestamps: true
+    }
+)
+
+userSchema.virtual("fullName").get(function () {
+    return `${this.name.first} ${this.name.last}`;
+});
+
+userSchema.pre("save", function(next) {
+    let user = this;
+    if(user.subscribedAccount == undefined) {
+        Subscriber.findOne({
+            email: user.email
+        })
+        .then(subscriber => {
+            user.subscribedAccount = subscriber;
+            next();
+        })
+        .catch(error => {
+            console.log(`error in associating subscribe: ${error.message}`);
+            next(error);
+        })
+    }
+    else {
+        next();
+    }
+})
 
 module.exports = mongoose.model("User", userSchema);
 
