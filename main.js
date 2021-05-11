e0a256cfff8c354796fd4079545601cad38aac76
isaiahhaley0 <isaiah.haley@ucdenver.edu>
2021-05-11 17:42:53 -0600

    mongodb update

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ router.use((req, res, next) => {
 });
 
 
-mongoose.connect("mongodb://localhost:27017/vibezdb",{useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser: true, useUnifiedTopology: true })
 
 app.set("port",process.env.PORT||3000);
 
